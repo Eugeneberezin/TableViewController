70fdcce9be56170d2465552659b919733e2c435c
Eugene Berezin <berezin.nlp@gmail.com>
2019-09-24 18:27:24 -0700

    Add share button

--- TableViewController/TableViewController/DetailViewController.swift
+++ TableViewController/TableViewController/DetailViewController.swift
@@ -20,6 +20,7 @@ class DetailViewController: UIViewController {
         
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(handleShare))
         
         
         if let imageToLoad = selectedImage {
@@ -43,14 +44,15 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    @objc func handleShare() {
+         guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+               print("No image found")
+               return
+           }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+           let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+           vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+           present(vc, animated: true)
     }
-    */
 
 }
