f242597e9f8fb866ff81f099e0cb0e09cec66eb5
Eugene Berezin <berezin.nlp@gmail.com>
2019-09-26 05:36:10 -0700

    try XCUITest

--- TableViewController/TableViewControllerUITests/TableViewControllerUITests.swift
+++ TableViewController/TableViewControllerUITests/TableViewControllerUITests.swift
@@ -11,12 +11,12 @@ import XCTest
 class TableViewControllerUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+       
         continueAfterFailure = false
+        let app = XCUIApplication()
+              app.launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
     }
 
     override func tearDown() {
@@ -25,9 +25,9 @@ class TableViewControllerUITests: XCTestCase {
 
     func testExample() {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-        
+        XCUIApplication().tables/*@START_MENU_TOKEN@*/.staticTexts["nssl0046.jpg"]/*[[".cells.staticTexts[\"nssl0046.jpg\"]",".staticTexts[\"nssl0046.jpg\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+              
+                        
                         
 
         // Use recording to get started writing UI tests.
